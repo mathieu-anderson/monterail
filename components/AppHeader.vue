1470f6ee5b6f2365d92945eeb4d0abceb60bffcc
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-04-03 17:23:22 +1200

    Finish first version of header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -10,7 +10,7 @@
         QUESTIONS
       </div>
       <div class="symbol_questions">
-        +
+        <i class="ion-ios-plus"></i>
       </div>
       <div class="select_questions">
         <input type="radio" name="myShelf" value="">
@@ -23,11 +23,13 @@
         </input>
       </div>
     </section>
-    <section class="sort">
-      Sort by <a href="#" class="">recent</a> or <a href="#">hot</a>
+    <section class="sortQuestions">
+      <div class="sort">
+        Sort by: <a href="#" class="">recent</a> or <a href="#">hot</a>
+      </div>
     </section>
     <section class="searchField">
-      <input type="text" name="searchQuestion" placeholder="Search questions" value="">
+      <input class= "search" type="text" name="searchQuestion" placeholder="Search questions" value="">
     </section>
     <section class="searchButton">
       <div class="headerButton">SEARCH</div>
@@ -35,19 +37,19 @@
   </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../assets/css/main.scss';
 
   header {
     background-color: $white;
     width: 100vw;
+    padding: 1em 0em 0em 0em;
     display: grid;
     grid-template-columns: [col1start] 15vw [col2start] 1fr [col3start] 20vw [col3end];
-    grid-template-rows: [row1] 10vh [row2] 7vh;
+    grid-template-rows: [row1] 10vh [row2] 1fr;
   }
 
   .back {
-    border: 1px dotted red;
     grid-column-start: col1start;
     grid-column-end: col2start;
     grid-row-start: row1;
@@ -66,7 +68,6 @@
   }
 
   .questions {
-    border: 1px dotted red;
     grid-column-start: col2start;
     grid-column-end: col3start;
     grid-row-start: row1;
@@ -76,45 +77,63 @@
     align-items: center;
     align-content: stretch;
       .title_questions {
+        text-align: left;
+        font-size: 2em;
+        font-weight: bolder;
+        padding: 1em 1em 1em 0em;
         flex: 0 1 auto;
-        border: 1px dotted blue;
       }
       .symbol_questions {
+        font-size: 2.5em;
+        color: $blue;
         flex: 1;
-        border: 1px dotted blue;
       }
       .select_questions {
+        text-align: right;
         flex: 0.5;
-        border: 1px dotted blue;
       }
   }
 
-  .sort {
-    border: 1px dotted red;
+  .sortQuestions {
+    display: flex;
+    align-items: center;
     grid-column-start: col3start;
     grid-column-end: col3end;
     grid-row-start: row1;
     grid-row-end: row1;
+    .sort{
+      flex: 1;
+      text-align: center;
+      font-style: italic;
+    }
   }
 
   .searchField {
-    border: 1px dotted red;
+    display: flex;
+    align-content: stretch;
     grid-column-start: col2start;
     grid-column-end: col3start;
     grid-row-start: row2;
     grid-row-end: row2;
+    padding: 0em 0em 1em 0em;
+    .search {
+      flex: 1;
+      background-color: $light-gray;
+      padding: 0em 0em 0em 1em;
+    }
   }
 
   .searchButton {
-    border: 1px dotted red;
     grid-column-start: col3start;
     grid-column-end: col3end;
     grid-row-start: row2;
     grid-row-end: row2;
+    padding: 0em 2em 1em 2em;
   }
 
   .headerButton {
     border: 1px solid $dark-gray;
+    background-color: $white;
     color: $dark-gray;
     padding: 0.5em;
     text-align: center;
